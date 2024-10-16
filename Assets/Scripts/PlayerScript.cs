83de60855167cb0f9f7d3c54fd279789f3ea7b51
Taylor-Henson <551726@chrome.escg.ac.uk>
2024-10-16 17:26:06 +0100

    animation changes 16/10/2024

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -10,11 +12,17 @@ public class PlayerScript : MonoBehaviour
     private int speed = 8;
     private int jumpForce = 20;
 
+    //groundcheck
+    public bool touchingGround = false;
+
     [Header("References")]
     //components
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sr;
+
+    //layers
+    LayerMask groundLayerMask;
 
     #endregion
     #region start and update
@@ -26,6 +34,9 @@ public class PlayerScript : MonoBehaviour
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
 
+        //layers
+        groundLayerMask = LayerMask.GetMask("Ground");
+
         //game objects
     }
 
@@ -36,6 +47,7 @@ public class PlayerScript : MonoBehaviour
         Movement();
         Jump();
         Animations();
+        GroundCheck();
     }
     #endregion
 
@@ -54,18 +66,37 @@ public class PlayerScript : MonoBehaviour
             sr.flipX = false;
         }
 
-        //sanim.SetBool("Idle", true);
         //makes the run animation play if the player is moving and end when they stop
-        if (rb.velocityX > 0 || rb.velocityX < 0)
+        if (rb.velocityX > 0 || rb.velocityX < 0 && touchingGround)
         {
-            //anim.SetBool("Idle", false);
-            //anim.SetBool("Run", true);
+            anim.SetBool("Run", true);
         }
         else
         {
-            //anim.SetBool("Idle", true);
-            //anim.SetBool("Run", false);
-        }        
+            anim.SetBool("Run", false);
+        }
+
+        //makes the jump animation override others if the space key is pressed
+        if (rb.velocity.y > 1)
+        {
+            Debug.Log("Jumping");
+            anim.SetBool("Jump", true);
+        }
+
+        else if (rb.velocity.y < 0)
+        {
+            Debug.Log("falling;");
+            anim.SetBool("Fall", true);
+            anim.SetBool("Jump", false);
+        }
+
+        //landing on ground
+        if (touchingGround)
+        {
+            Debug.Log("landing");
+            anim.SetBool("Jump", false);
+            anim.SetBool("Fall", false);
+        }
     }
     #endregion
 
@@ -86,10 +117,42 @@ public class PlayerScript : MonoBehaviour
     void Jump()
     {
         //press space to apply a uppwards impulse force
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space") && touchingGround)
         {
             rb.AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode2D.Impulse);
         }
     }
+    #endregion
+
+    #region checks
+    //where all checks occur
+
+    //performs a check using Tags to see if the player is touching the ground
+    private void GroundCheck()
+    {
+        //raycast variables
+        float rayLength = 1.5f;
+        Vector2 position = transform.position;
+        Vector2 direction = Vector2.down;
+        RaycastHit2D hit;
+
+        //the raycast
+        
+        hit = Physics2D.Raycast(position, direction, rayLength, groundLayerMask);
+        //visual raycast
+        Debug.DrawRay(position, direction, Color.green);
+        
+        //if raycast hits ground
+        if (hit.collider != null)
+        {
+            touchingGround = true;
+            //Debug.Log("grounded");
+        }
+        else
+        {
+            touchingGround = false;
+        }
+    }
+
     #endregion
 }
